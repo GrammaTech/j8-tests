9f8e13e641d2cde1fae4665b148187d4a33120fe
Anurag Singh <asingh@grammatech.com>
2017-03-28 12:42:56 -0400

    updated conftest

--- test_framework/conftest.py
+++ test_framework/conftest.py
@@ -5,36 +5,30 @@ create a global function which can be used in every subdir
 import pytest
 import glob
 import os
-import itertools
 
-@pytest.fixture(scope='session')
-def tools():
-    '''
-        get all the tools directory
-    '''
-    tools_list = glob.glob('src/tools/*')
-    return tools_list
+def pytest_addoption(parser):
+    ''' user defined values '''
+    parser.addoption("--tool", action="append", help="pass the tools name")
+    parser.addoption("--tool_path", action="append",\
+            help="pass the path to tool")
+    parser.addoption("--conf_file", help="run using config")
 
-@pytest.fixture(scope='session')
-def apps():
-    '''
-        get all the apps dir
-    '''
-    apps_list = glob.glob('src/apps/*')
-    return apps_list
+@pytest.fixture
+def tool(request):
+    ''' user defined tool '''
+    return request.config.getoption("--tool")
 
-def combinations():
-    '''
-        cross product of tools and apps
-    '''
-    tools_list = [os.path.basename(x) for x in glob.glob('src/tools/*')]
-    apps_list = [os.path.basename(x) for x in glob.glob('src/apps/*')]
-    return [tup for tup in itertools.product(tools_list, apps_list)]
+@pytest.fixture
+def tool_path(request):
+    ''' user defined tool path, convert to class path'''
+    return request.config.getoption("--tool_path")
 
 def pytest_namespace():
     '''
         pytest global variable
     '''
-    return {'tool_app': combinations()}
-
-
+    return {
+            'apps_list': [os.path.basename(x) for x in\
+                glob.glob('src/apps/*')],
+            'root_dir' : os.getcwd(),
+            }
