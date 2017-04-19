636833681c4f351523a7cb3d95cbfe71611ba8c4
Brian Alliet <balliet@grammatech.com>
2017-04-19 11:04:32 -0400

    Comments and ':' => File.separatorChar

--- test_framework/tests/callgraph/SootCG.java
+++ test_framework/tests/callgraph/SootCG.java
@@ -6,29 +6,38 @@ import soot.jimple.toolkits.callgraph.*;
 import soot.options.Options;
 
 public class SootCG {
-    public static String toString(SootMethod m)
-    {
-        return m.getDeclaringClass().getName() + "."
-             + m.getName()
-             + AbstractJasminClass.jasminDescriptorOf(m.makeRef());
-    }
+    // args == [jre path, app jar 1, ..., app jar n, entrypoint]
     public static void main(String[] args) {
         String jre_jars = args[0];
+
+        // Build the class path (we have all the components separated out, but
+        // soot wants a ':' separated string
         StringBuilder sb = new StringBuilder();
         for(int i=1;i<args.length-1;i++)
-            sb.append(args[i]).append(":");
+            sb.append(args[i]).append(File.separatorChar);
         sb.append(jre_jars).append(File.separatorChar).append("rt.jar");
-        sb.append(':');
+        sb.append(File.separatorChar);
         sb.append(jre_jars).append(File.separatorChar).append("jce.jar");
         Options.v().set_soot_classpath(sb.toString());
+
+        // Main class is the final arguments
         Options.v().classes().add(args[args.length-1]);
+        
         Options.v().set_whole_program(true);
         Scene.v().loadNecessaryClasses();
         PackManager.v().runPacks();
+        
         CallGraph cg = Scene.v().getCallGraph();
         for(Iterator<Edge> it = cg.iterator(); it.hasNext(); ) {
             Edge e = it.next();
             System.out.println(toString(e.src()) + " -> " + toString(e.tgt()));
         }
     }
+
+    public static String toString(SootMethod m)
+    {
+        return m.getDeclaringClass().getName() + "."
+             + m.getName()
+             + AbstractJasminClass.jasminDescriptorOf(m.makeRef());
+    }    
 }
