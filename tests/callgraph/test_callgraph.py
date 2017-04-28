e8d168364504559636980d2a86bcb53fdd2be619
Anurag Singh <asingh@grammatech.com>
2017-04-28 11:47:39 -0400

    moving adapter compilation to conftest

--- tests/callgraph/test_callgraph.py
+++ tests/callgraph/test_callgraph.py
@@ -13,34 +13,6 @@ import glob #glob.glob
 sys.path.append(os.path.join(pytest.root_dir, 'tests'))
 import utils
 
-
-
-@pytest.fixture(scope="module")
-def adapter(request):
-    return xadapter(*request.param)
-
-@utils.change_dir(os.path.dirname(__file__))
-def xadapter(tool_name, tool_path):
-    '''
-        builds the adapter
-        It takes the tool_path, gets the class path
-        The change_dir ensures the adapter is build
-        in the same directory
-
-    '''
-    adapter_name = tool_name + 'CGAdapter';
-    # set class path
-    class_path = utils.generate_classpath(tool_name, tool_path)
-    # build adaptor
-    cmd = ['javac', '-cp', class_path, adapter_name + '.java']
-    # run the adapter cmd
-    _, _, returncode = utils.run_cmd(cmd)
-    # check if the build passed
-    assert returncode == 0
-    return (class_path, adapter_name)
-    #os.remove(glob.glob(adapter_name + "*.class"))
-
-
 def test_callgraph(adapter,app):
     xtest_callgraph(adapter,app)
 
