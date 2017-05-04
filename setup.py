bc89007f4f42e847e24df5b22d0fb3887864af3e
Brian Alliet <balliet@grammatech.com>
2017-05-04 17:44:34 -0400

    remove all version/os checks from setup.py (here me out... see below)
    1) The OS check is unnecessary. The tests actually work on windows and there
       is nothing os specific in here
    
    2) The java version check is unnecceary. Our adapters depend on exactly
       the same java version as is used to compile the tools themselves. If
       your java is capable of building the tool itself, you have the version
       required to compile the adapter. If your java can't build the tool,
       then the adapter build is going to fail anyway.
    
       At one point we inadvertantly depended on the java runtime jars
       included in the JRE you were running with. That isn't true anymore.
       We always use the jars provided with the test suite. This means
       you can run WALA with Java 7, but still analyze Java 8 code with
       Java 8 runtime jars.
    
    3) The python version check is unnecessary. We support all python
       versions supported by pytest itself (even 2.6). If you have an
       older python version, you don't have pytest, and you can't run
       the test suite anyway. (And what machines in the wild really have
       python 2.5 anyway...).

--- setup.py
+++ setup.py
@@ -12,46 +12,6 @@ import logging
 import time
 import pytest
 
-def run_check(logger):
-    '''
-        Run various checks
-        1) Operating system is Linux
-        2) Java 8 is installed
-        3) Python version 2.7 or greater is installed
-    '''
-    # check if operating system is Linux
-    if not sys.platform.startswith('linux'):
-        sys.exit('Error : Test suite only compatible with\
-                Linux operating system')
-
-    # check if at least python 2.7
-    status = sys.version_info >= (2, 7)
-    version = str(sys.version_info.major) + '.' + str(sys.version_info.minor)
-    if not status:
-        sys.exit('Error : python version atleast 2.7 not found')
-    logger.info('python version found %s ' % version)
-
-    # check if Java is installed
-    try:
-        proc = subprocess.Popen(['java', '-version'],\
-                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = proc.communicate()
-        # byte to str
-        stderr = stderr.decode("utf-8")
-        stdout = stdout.decode("utf-8")
-        # check if Java version is 8
-        try:
-            version = stderr.split('\n')[0].split(" ")[2]
-            logger.info('Java version found %s' % version)
-            if not '1.8' in version:
-                sys.exit('Error : Java 8 not found')
-        except:
-            logger.error(sys.exc_info())
-            sys.exit('Error : Java 8 not found')
-    except:
-        logger.error(sys.exc_info())
-        sys.exit('Error : Java not found')
-
 if __name__ == "__main__":
     # configure logging
     logger = logging.getLogger('JAVA8_Tests')
@@ -78,9 +38,6 @@ if __name__ == "__main__":
         # add the handlers to the logger
         logger.addHandler(ch)
 
-    logger.info('Running System Check')
-    # check system requirements
-    run_check(logger)
     # call pytest in current directory using '.'
     sys.argv.extend('.');
     sys.exit(pytest.main(sys.argv[1:]))
