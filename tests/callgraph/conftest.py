8b1ac81830643a33f5e5c4d56f8562dc1f1d058d
Brian Alliet <balliet@grammatech.com>
2017-07-11 17:10:52 -0400

    Add a BSD license to all source files

--- tests/callgraph/conftest.py
+++ tests/callgraph/conftest.py
@@ -1,3 +1,41 @@
+# This material is based upon work supported by the United States Air Force
+# and DARPA under Contract No. FA8750-15-C-0082. Any opinions, findings and
+# conclusions or recommendations expressed in this material are those of
+# the author(s) and do not necessarily reflect the views of the United
+# States Air Force and DARPA.
+#
+#
+# Copyright 2017 GrammaTech, Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from this
+# software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# If you are interested in making contributions, then please contact
+# info@grammatech.com
 '''
     Adapter fixture for the callgraph tests
     See https://github.com/GrammaTech/j8-tests/blob/master/Readme.md#callgraph-ir
